957b457834ac76ec0195edd19839a97e0ef94c6a
katzur4 <johnwesley.valentino@gmail.com>
2024-06-01 22:11:40 +0800

    a testing javascript dom

---
+++ index.js
@@ -0,0 +1,16 @@
+// get element by ID
+const title = document.getElementById('main-heading');
+
+console.log(title);
+//  get element by className
+const listItem = document.getElementsByClassName('list-items');
+
+console.log(listItem);
+
+//querySelector()
+const container = document.querySelector('div');
+console.log(container);
+
+//alert
+alert('Hello Wrold');
+
